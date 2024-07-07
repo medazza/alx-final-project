b3018c997f4b63317bad918a08bc30bb7bb6845f
medazza <midazza27@gmail.com>
2024-07-07 16:09:50 +0100

    configur JWT

--- SocioHub/settings.py
+++ SocioHub/settings.py
@@ -40,9 +40,20 @@ INSTALLED_APPS = [
 
     # apps
     'core.apps.CoreConfig',
+    'core.user.apps.UserConfig',
 
     # third-party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS':
+    ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -137,3 +148,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
