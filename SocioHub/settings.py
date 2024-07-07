c761de16a02f593d0e7c7bc6256389f299c08bf3
medazza <midazza27@gmail.com>
2024-07-07 13:37:04 +0100

    create the user app

--- SocioHub/settings.py
+++ SocioHub/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'core.apps.CoreConfig',
+
+    # third-party apps
 ]
 
 MIDDLEWARE = [
